962576f33c1d5d2e7b7c82326b1d8928ea9f58bb
Tim <timsoual@gmail.com>
2019-09-05 16:53:28 +0200

    Add form validation and styles

--- src/app/main-form/main-form.component.ts
+++ src/app/main-form/main-form.component.ts
@@ -13,11 +13,20 @@ export class MainFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // We initialize the from group with validators.
     this.mainForm = new FormGroup({
       'defectType': new FormControl(null, Validators.required),
-      'operatorName': new FormControl(null, Validators.required),
+      'operatorName': new FormControl(null, [Validators.required, this.validateOperatorName.bind(this)]),
       'comments': new FormControl(null)
     });
+  }
+
+  validateOperatorName(control: FormControl): {[s: string]: boolean} {
+    // This validator sets the nameHasNumber error to true if the operator name has a number in it.
+    if (/\d/.test(control.value)) {
+      return {'nameHasNumber': true};
+    }
+    return null;
   }
 
   onSubmit() {
